4659d13e400ac073263b0771968a8927e6c05c7e
DSMT2 <87914274+DSMT2@users.noreply.github.com>
2021-07-24 22:22:02 +0100

    Update CyanEmuPlayerController.cs
    changed arm positions and rotations to be more accurate with VRChat's desktop arm positions and rotations

--- CyanEmu/Scripts/CyanEmuPlayerController.cs
+++ CyanEmu/Scripts/CyanEmuPlayerController.cs
@@ -170,15 +170,15 @@ namespace VRCPrefabs.CyanEmu
 
             rightArmPosition_ = new GameObject("Right Arm Position");
             rightArmPosition_.transform.SetParent(playerCamera_.transform, false);
-            rightArmPosition_.transform.localPosition = new Vector3(0.2f, -0.2f, 0.75f);
-            rightArmPosition_.transform.localRotation = Quaternion.Euler(-45, 0, -90);
+            rightArmPosition_.transform.localPosition = new Vector3(0.1527f, -0.029f, 0.4180f);
+            rightArmPosition_.transform.localRotation = Quaternion.Euler(-35, 0, -90);
             rightArmRigidbody_ = rightArmPosition_.AddComponent<Rigidbody>();
             rightArmRigidbody_.isKinematic = true;
             
             leftArmPosition_ = new GameObject("Left Arm Position");
             leftArmPosition_.transform.SetParent(playerCamera_.transform, false);
-            leftArmPosition_.transform.localPosition = new Vector3(-0.2f, -0.2f, 0.75f);
-            leftArmPosition_.transform.localRotation = Quaternion.Euler(-45, 0, -90);
+            leftArmPosition_.transform.localPosition = new Vector3(-0.1473f, 0.0025f, 0.4180f);
+            leftArmPosition_.transform.localRotation = Quaternion.Euler(-35, 0, -90);
             leftArmRigidbody_ = leftArmPosition_.AddComponent<Rigidbody>();
             leftArmRigidbody_.isKinematic = true;
             
